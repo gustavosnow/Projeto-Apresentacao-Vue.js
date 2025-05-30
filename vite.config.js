fe697b966d46f986c8fc45c0de4b3a718cab3350
Gustavo <nevesgustavo204@gmail.com>
2025-05-30 10:23:13 -0300

    adiciona configuração do servidor para permitir acesso pela rede local

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   base: process.env.NODE_ENV === 'production' ? '/projetoPW/' : '/',
   plugins: [vue()],
+  server: {
+    port: 5173,
+    host: true // permite acesso pela rede local
+  }
 })
